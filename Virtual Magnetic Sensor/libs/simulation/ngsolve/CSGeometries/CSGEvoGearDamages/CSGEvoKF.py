1d891624e16ee697601ac78b8be6c3a7ae40df69
JMC1805 <mcneill.jonathan@outlook.com>
2024-07-05 03:03:47 +0200

    Updated KF DMG Model to handle non rounded objects

--- Virtual Magnetic Sensor/libs/simulation/ngsolve/CSGeometries/CSGEvoGearDamages/CSGEvoKF.py
+++ Virtual Magnetic Sensor/libs/simulation/ngsolve/CSGeometries/CSGEvoGearDamages/CSGEvoKF.py
@@ -13,7 +13,7 @@ class CSGEvoKF:
         body(csg.Solid):  Netgen.csg.CSGeometry representation of the gear damage which is being used for the simulation.
 
     Raises:
-        ValueError: Informs the user that the give tooth number is not being rendered and should be changed to get correct results. 
+        ValueError: Informs the user that the give tooth number is not being rendered and should be changed to get correct results.
     """
 
     CSGEvoGear_cls: CSGEvoGear
@@ -21,11 +21,11 @@ class CSGEvoKF:
 
     def __init__(self,
                  EvoTooth_ini: EvoGear) -> None:
-        """Initalize the cold flow gear geometry using the netgen.csg.CSGeometry elements. 
+        """Initalize the cold flow gear geometry using the netgen.csg.CSGeometry elements.
 
         Args:
             EvoTooth_ini (EvoTooth): Class is being used to calculated all the parameters for the tooth damage.
-            tooth_number (int): On which tooth the damage is being placed, begining with the horizonal tooth zero at the right side and counting in a counterclockwise direction. 
+            tooth_number (int): On which tooth the damage is being placed, begining with the horizonal tooth zero at the right side and counting in a counterclockwise direction.
             c (float): Cold flow depth indicated in millimeter.
             tooth_side (str): On whiche tooth side the damage is being placed the available options are: left or right.
         """
@@ -41,7 +41,7 @@ class CSGEvoKF:
 
         Args:
             involute_points (int): The points which are being used to calculate the involute-function.
-            tooth_number (int): On which tooth the damage is being placed, begining with the horizonal tooth zero at the right side and counting in a counterclockwise direction. 
+            tooth_number (int): On which tooth the damage is being placed, begining with the horizonal tooth zero at the right side and counting in a counterclockwise direction.
             c (float): Cold flow depth indicated in millimeter.
             tooth_side (str): On which tooth side the damage is being placed the available options are: left or right.
 
@@ -53,8 +53,8 @@ class CSGEvoKF:
             involute_points, tooth_number, c, tooth_side)
         # Front & Back Plane
         back = csg.Plane(
-            csg.Pnt(0, 0, (-1)*self.CSGEvoGear_cls.EvoTooth_ini.length), csg.Vec(0, 0, -1))
-        front = csg.Plane(csg.Pnt(0, 0, 0), csg.Vec(0, 0, 1))
+            csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]+(-1)*self.CSGEvoGear_cls.EvoTooth_ini.length), csg.Vec(0, 0, -1))
+        front = csg.Plane(csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]), csg.Vec(0, 0, 1))
         # Buidling teeth
         for i in range(len(extrude_list)):
             if i == 0:
@@ -66,124 +66,88 @@ class CSGEvoKF:
                                               extrude_list[i][2],
                                               csg.Vec(extrude_list[i][3]))
         # Building GearBody
-        gearbody = csg.Cylinder(csg.Pnt(0, 0, 0),
-                                csg.Pnt(
-                                    0, 0, (-1)*self.CSGEvoGear_cls.EvoTooth_ini.length),
-                                self.CSGEvoGear_cls.EvoTooth_ini.d_f/2)-csg.Cylinder(csg.Pnt(0, 0, 0),
-                                                                                     csg.Pnt(
-                                                                                         0, 0, (-1)*self.CSGEvoGear_cls.EvoTooth_ini.length),
-                                                                                     self.CSGEvoGear_cls.EvoTooth_ini.diameter[0])
+        if self.CSGEvoGear_cls.EvoTooth_ini.diameter[0] != 0:
+            gearbody = csg.Cylinder(csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]),
+                                    csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]+(-1)*self.CSGEvoGear_cls.EvoTooth_ini.length),
+                                    self.CSGEvoGear_cls.EvoTooth_ini.d_f/2) \
+                - csg.Cylinder(csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]),
+                               csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]+(-1)*self.CSGEvoGear_cls.EvoTooth_ini.length),
+                               self.CSGEvoGear_cls.EvoTooth_ini.diameter[0])
+        else:
+            gearbody = csg.Cylinder(csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]),
+                                    csg.Pnt(self.CSGEvoGear_cls.EvoTooth_ini.pos[0], self.CSGEvoGear_cls.EvoTooth_ini.pos[1],self.CSGEvoGear_cls.EvoTooth_ini.pos[2]+(-1)*self.CSGEvoGear_cls.EvoTooth_ini.length),
+                                    self.CSGEvoGear_cls.EvoTooth_ini.d_f/2)
+            
         return (csg_evotooth+gearbody)*front*back
-
+            
     def evokf_2d_point_array(self, involute_points: int, c: float, tooth_side: str) -> np.array:
-        """Method to generate the 2d spline of cold flow damage.
+        tooth_side_coord = self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[2][::-1, :]
+        tooth_side_coord = np.append(tooth_side_coord, np.array([[tooth_side_coord[-1, 0]], [self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]]).T, axis=0)
 
-        Args:
-            tooth_number (int): On which tooth the damage is being placed, begining with the horizonal tooth zero at the right side and counting in a counterclockwise direction. 
-            c (float): Cold flow depth indicated in millimeter.
-            tooth_side (str): On which tooth side the damage is being placed the available options are: left or right.
+        v = np.reshape(tooth_side_coord[-1]-tooth_side_coord[0], (2, 1))
+        v = c*(1/(np.sqrt(np.power(v[0], 2)+np.power(v[1], 2))))*v
+        v_ = np.dot(self.CSGEvoGear_cls.EvoTooth_ini.rotation_matrix(np.radians(-90))[0:2, 0:2], v)
+        diff = tooth_side_coord[tooth_side_coord.shape[0]//2]+v_.T
+        p2 = diff+v.T
+        m_c = (p2[0, 1]-diff[0, 1])/(p2[0, 0]-diff[0, 0])
 
-        Returns:
-            np.array: Array of 2d points.
-        """
-        match tooth_side:
-            case "right":
-                tooth_side_coord = self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[
-                    2][::-1, :]
-                tooth_side_coord = np.append(tooth_side_coord, np.array(
-                    [[tooth_side_coord[-1, 0]], [self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]]).T, axis=0)
+        sol = []
+        for i in range(1, tooth_side_coord.shape[0]):
+            if i < tooth_side_coord.shape[0]-1:
+                m_int = (tooth_side_coord[i, 1]-tooth_side_coord[i-1, 1])/(tooth_side_coord[i, 0]-tooth_side_coord[i-1, 0])
+                x = (tooth_side_coord[i-1, 1]-diff[0, 1]+m_c * diff[0, 0]-m_int*tooth_side_coord[i-1, 0])/(m_c-m_int)
+                if tooth_side_coord[i-1, 0] <= x <= tooth_side_coord[i, 0]:
+                    y = m_c*(x-diff[0, 0])+diff[0, 1]
+                    sol.append([x, y, i])
+            else:
+                y = m_c*(tooth_side_coord[-1, 0]-diff[0, 0])+diff[0, 1]
+                if tooth_side_coord[i-1, 1] >= y >= tooth_side_coord[i, 1]:
+                    sol.append([tooth_side_coord[i-1, 0], y, i])
 
-                v = np.reshape(
-                    tooth_side_coord[-1]-tooth_side_coord[0], (2, 1))
-                v = c*(1/(np.sqrt(np.power(v[0], 2)+np.power(v[1], 2))))*v
-                v_ = np.dot(self.CSGEvoGear_cls.EvoTooth_ini.rotation_matrix(
-                    np.radians(-90))[0:2, 0:2], v)
-                diff = tooth_side_coord[tooth_side_coord.shape[0]//2]+v_.T
-                p2 = diff+v.T
-                m_c = (p2[0, 1]-diff[0, 1])/(p2[0, 0]-diff[0, 0])
-
-                sol = []
-                for i in range(1, tooth_side_coord.shape[0]):
-                    if i < tooth_side_coord.shape[0]-1:
-                        m_int = (tooth_side_coord[i, 1]-tooth_side_coord[i-1, 1])/(
-                            tooth_side_coord[i, 0]-tooth_side_coord[i-1, 0])
-                        x = (tooth_side_coord[i-1, 1]-diff[0, 1]+m_c *
-                             diff[0, 0]-m_int*tooth_side_coord[i-1, 0])/(m_c-m_int)
-                        if tooth_side_coord[i-1, 0] <= x <= tooth_side_coord[i, 0]:
-                            y = m_c*(x-diff[0, 0])+diff[0, 1]
-                            sol.append([x, y, i])
-                    else:
-                        y = m_c*(tooth_side_coord[-1, 0]-diff[0, 0])+diff[0, 1]
-                        if tooth_side_coord[i-1, 1] >= y >= tooth_side_coord[i, 1]:
-                            sol.append([tooth_side_coord[i-1, 0], y, i])
-                sol = np.array(sol)
-                sol = sol[::-1, :]
-                indices = sol[::-1, 2].tolist()
-                for i in range(sol.shape[0]):
-                    tooth_side_coord = np.insert(tooth_side_coord, int(
-                        sol[i, 2]), np.reshape(sol[i, [0, 1]], (1, 2)), axis=0)
-
-                temp = tooth_side_coord[0:int(indices[0]+1), ::]
-                temp_1 = tooth_side_coord[int(indices[1]+1):, ::]
-                tooth_side_coord = np.vstack((temp, temp_1))
-
-                tooth_side_coord = np.vstack((self.CSGEvoGear_cls.evotooth_2dpoint_array(
-                    involute_points)[1], tooth_side_coord))
-
-                tooth_side_coord = np.vstack((tooth_side_coord, [
-                                             (-1)*tooth_side_coord[-1, 0], self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]))
-                return tooth_side_coord
-
-            case "left":
-                tooth_side_coord = self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[
-                    1][::-1, :]
-                tooth_side_coord = np.append(tooth_side_coord, np.array(
-                    [[tooth_side_coord[-1, 0]], [self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]]).T, axis=0)
-                tooth_side_coord = tooth_side_coord[::-1, :]
-
-                v = np.reshape(
-                    tooth_side_coord[-1]-tooth_side_coord[0], (2, 1))
-                v = c*(1/(np.sqrt(np.power(v[0], 2)+np.power(v[1], 2))))*v
-                v_ = np.dot(self.CSGEvoGear_cls.EvoTooth_ini.rotation_matrix(
-                    np.radians(-90))[0:2, 0:2], v)
-
-                diff = tooth_side_coord[tooth_side_coord.shape[0]//2]+v_.T
-                p2 = diff+v.T
-                m_c = (p2[0, 1]-diff[0, 1])/(p2[0, 0]-diff[0, 0])
-
-                sol = []
-
-                for i in range(1, tooth_side_coord.shape[0]):
-                    if i > 1:
-                        m_int = (tooth_side_coord[i, 1]-tooth_side_coord[i-1, 1])/(
-                            tooth_side_coord[i, 0]-tooth_side_coord[i-1, 0])
-                        x = (tooth_side_coord[i-1, 1]-diff[0, 1]+m_c *
-                             diff[0, 0]-m_int*tooth_side_coord[i-1, 0])/(m_c-m_int)
-                        y = m_c*(x-diff[0, 0])+diff[0, 1]
-                        if tooth_side_coord[i-1, 0] <= x <= tooth_side_coord[i, 0]:
-                            sol.append([x, y, i])
-                    elif i == 1:
-                        y = m_c*(tooth_side_coord[0, 0]-diff[0, 0])+diff[0, 1]
-                        if tooth_side_coord[i-1, 1] <= y <= tooth_side_coord[i, 1]:
-                            sol.append([tooth_side_coord[0, 0], y, i])
-
-                sol = np.array(sol)
-                sol = sol[::-1, :]
-                indices = sol[::-1, 2].tolist()
-
-                for i in range(sol.shape[0]):
-                    tooth_side_coord = np.insert(tooth_side_coord, int(
-                        sol[i, 2]), np.reshape(sol[i, [0, 1]], (1, 2)), axis=0)
-
-                temp = tooth_side_coord[0:int(indices[0]+1), ::]
-                temp_1 = tooth_side_coord[int(indices[1]+1):, ::]
-                tooth_side_coord = np.vstack((temp, temp_1))
-                tooth_side_coord = np.vstack(
-                    (tooth_side_coord, self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[2][::-1, :]))
-                tooth_side_coord = np.vstack((tooth_side_coord, [
-                                             tooth_side_coord[-1, 0], self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]))
-
-                return tooth_side_coord         
+        sol = np.array(sol)
+        sol = sol[::-1, :]
+        indices = sol[::-1, 2].tolist()
+        
+        if len(indices) == 2:
+            for i in range(sol.shape[0]):
+                tooth_side_coord = np.insert(tooth_side_coord, int(sol[i, 2]), np.reshape(sol[i, [0, 1]], (1, 2)), axis=0)
+            
+            temp = tooth_side_coord[0:int(indices[0]+1), ::]
+            temp_1 = tooth_side_coord[int(indices[1]+1):, ::]
+            tooth_side_coord = np.vstack((temp, temp_1))
+            match tooth_side:
+                case "right":
+                    tooth_side_coord = np.vstack((self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[1], tooth_side_coord))
+                    tooth_side_coord = np.vstack((tooth_side_coord, [(-1)*tooth_side_coord[-1, 0], self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]))
+                    return tooth_side_coord
+                case "left":
+                    lst_entry_ts_coord=np.copy(tooth_side_coord[-1])
+                    tooth_side_coord=tooth_side_coord[0:-1,::]
+                    tooth_side_coord[::,0]=tooth_side_coord[::-1,0]*(-1)
+                    tooth_side_coord[::,1]=tooth_side_coord[::-1,1]
+                    tooth_side_coord = np.vstack((tooth_side_coord,self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[2][::-1]))
+                    tooth_side_coord = np.vstack((tooth_side_coord,lst_entry_ts_coord))
+                    tooth_side_coord = np.vstack((tooth_side_coord, [(-1)*tooth_side_coord[-1, 0], self.CSGEvoGear_cls.EvoTooth_ini.d_f/2*0.99]))
+                    return tooth_side_coord
+        elif len(indices)==1:
+            for index,entry in enumerate(tooth_side_coord):
+                if index > 0:
+                    v = np.reshape(tooth_side_coord[index]-tooth_side_coord[index-1], (2, 1))
+                    v = c*(1/(np.sqrt(np.power(v[0], 2)+np.power(v[1], 2))))*v
+                    v_ = np.dot(self.CSGEvoGear_cls.EvoTooth_ini.rotation_matrix(np.radians(-90))[0:2, 0:2], v)
+                    v_ = np.reshape(v_, (1, 2))
+                    tooth_side_coord[index] = tooth_side_coord[index]+v_
+                    if index==1:
+                        tooth_side_coord[0,0]=tooth_side_coord[0,0]+v_[0,0]
+            match tooth_side:
+                case "right":
+                    tooth_side_coord = np.vstack((self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[1], tooth_side_coord))
+                    return tooth_side_coord
+                case "left":
+                    tooth_side_coord[::,0]=tooth_side_coord[::-1,0]*(-1)
+                    tooth_side_coord[::,1]=tooth_side_coord[::-1,1]
+                    tooth_side_coord = np.vstack((tooth_side_coord,self.CSGEvoGear_cls.evotooth_2dpoint_array(involute_points)[2][::-1]))
+                    return tooth_side_coord
 
     def evokf_extrude_list(self, involute_points: int, tooth_number: int, c: float, tooth_side: str) -> list:
         """Method to put the cold flow damage in the list which indicates what every tooth is and clearly defines it's position.
