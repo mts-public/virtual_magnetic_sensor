60cbaffb756aec9d9813ffbe860e8bb40cb4864b
Tim Becker <tim.becker@mv.uni-kl.de>
2025-08-13 11:31:35 +0200

    removed theme

--- Virtual Magnetic Sensor/docs/source/conf.py
+++ Virtual Magnetic Sensor/docs/source/conf.py
@@ -5,7 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
